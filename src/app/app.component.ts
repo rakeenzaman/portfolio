7f5d932e89b60c90abac4068d6e639fed98a0b2b
rakeenzaman <rakeenzaman@gmail.com>
2025-02-28 08:42:07 -0600

    side nav thingy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { trigger, transition, style, animate, state } from '@angular/animations';
+import { trigger, transition, style, animate, state, animation, useAnimation } from '@angular/animations';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Experience } from './experience/experience.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -32,10 +33,10 @@ import { Experience } from './experience/experience.component';
       ])
     ]),
     trigger('header', [
-      state('true', style({ height: '101vh', marginTop: '0px', transform: 'scale(1.5)' })),
+      state('true', style({ height: '101vh', marginTop: '0px' })),
       state('false', style({ height: '300px', marginTop: '300px' })),
       transition('true <=> false', [
-        animate('500ms cubic-bezier(0,.31,.02,1)')
+        animate('1s cubic-bezier(0,.31,.02,1)')
       ])
     ]),
     trigger('fadeIn', [
@@ -60,28 +61,46 @@ import { Experience } from './experience/experience.component';
         animate('100ms ease-out', style({ opacity: 0 }))
       ])
     ]),
+    trigger('dynamicTransition', [
+      state('true', style({ opacity: 1, transform: 'translateY(0px)', filter: 'blur(0px)' })),
+      state('false', style({ opacity: 0, transform: 'translateY(100px)', filter: 'blur(5px)' })),
+      transition('false => true', [
+        animate('1s {{ delay }}ms cubic-bezier(0,.31,.02,1)')
+      ]),
+      transition('true => false', [
+        animate('0.5s cubic-bezier(0,.31,.02,1)')
+      ])
+    ]),
   ]
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements AfterViewInit {
   @ViewChild('exp') exp!: ElementRef;
+  @ViewChild('edu') edu!: ElementRef;
+  @ViewChild('about') about!: ElementRef;
   title = 'portfolio';
   atTopOfWindow = true;
   isLoading = true;
   showScrollDown = false;
 
-  ngOnInit() {
+  cspireHovering = false;
+  msuHovering = false;
+  internshipHovering = false;
+
+  divs = {aboutMe: 'about-me', education: 'education', experience: 'experience', projects: 'projects'};
+  currentDiv = this.divs.aboutMe;
+
+  ngAfterViewInit() {
     setTimeout(() => {
       window.scrollTo(0, 0);
       this.isLoading = false;
+      setTimeout(() => {
+        this.initObserver();
+      }, 0);
     }, 1000);
     console.log('App component initialized');
     window.scrollTo(0, 0);
     console.log(window.scrollY);
     window.addEventListener('scroll', this.onScroll);
-    window.onbeforeunload = function(event)
-    {
-      setTimeout(()=>{window.scrollTo(0, 0)},0);
-    };
 
     if (window.scrollY < 120) {
       document.body.style.overflow = 'hidden';
@@ -102,25 +121,28 @@ export class AppComponent implements OnInit, AfterViewInit {
     } else {
       this.atTopOfWindow = false;
     }
-    console.log(window.scrollY);
   };
 
-  ngAfterViewInit() {
-    const threshold = 0.2;
+  initObserver() {
+    const threshold = 0.1; 
     const observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                  console.log(entry);
-                    console.log('Element is fully visible in screen');
-                    //observer.disconnect();
-                } else {
-                    console.log('Element is not fully visible in screen');
-                }
-            });
+              if (entry.target === this.about.nativeElement && entry.isIntersecting) {
+                this.currentDiv = this.divs.aboutMe;
+              }
+              if (entry.target === this.exp.nativeElement && entry.isIntersecting) {
+                this.currentDiv = this.divs.experience;
+              }
+              if (entry.target === this.edu.nativeElement && entry.isIntersecting) {
+                this.currentDiv = this.divs.education;
+              }
+          });
         },
-        { threshold }
+        {rootMargin: '-49% 0% -49% 0%'}
     );
+    observer.observe(this.about.nativeElement);
     observer.observe(this.exp.nativeElement);
+    observer.observe(this.edu.nativeElement);
 }
 }
